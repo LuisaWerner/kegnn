66b5a124bda86f77332c72fe6ede7f27c1a9e424
luisaWerner <luisa.werner@icloud.com>
2022-03-10 16:43:11 +0100

    epochs

--- examples/nodeproppred/arxiv/train_and_evaluate.py
+++ examples/nodeproppred/arxiv/train_and_evaluate.py
@@ -30,7 +30,7 @@ def main():
     parser.add_argument('--hidden_channels', type=int, default=256)
     parser.add_argument('--dropout', type=float, default=0.5)
     parser.add_argument('--lr', type=float, default=0.01)
-    parser.add_argument('--epochs', type=int, default=100)  # 500
+    parser.add_argument('--epochs', type=int, default=10)  # 500
     parser.add_argument('--runs', type=int, default=1)  # 10
     parser.add_argument('--model', type=str, default='KENN_GCN')
     parser.add_argument('--mode', type=str, default='transductive')  # inductive/transductive
