7cf5fe9f154b3c516ef021616f0c86db75ab34f3
luisaWerner <luisa.werner@icloud.com>
2022-03-11 15:09:46 +0100

    bugfix batches

--- examples/nodeproppred/arxiv/training_batch.py
+++ examples/nodeproppred/arxiv/training_batch.py
@@ -1,10 +1,9 @@
 import time
 
 import torch
-from tqdm import tqdm
 
 
-def train(model, train_loader, optimizer, device, criterion, args, range_constraint):
+def train(model, train_loader, optimizer, device, criterion, range_constraint):
     """
     training loop - trains specified model by computing batches
     returns average epoch accuracy and loss
@@ -23,19 +22,18 @@ def train(model, train_loader, optimizer, device, criterion, args, range_constra
     total_examples = total_loss = total_correct = 0
 
     start = time.time()
-    for batch in tqdm(train_loader):
+    for batch in iter(train_loader):
         optimizer.zero_grad()
-        # batch = batch.to(device, 'edge_index')
         batch = batch.to(device)
-        out = model(batch.x, batch.edge_index, batch.relations)[:args.batch_size]
-        loss = criterion(out, batch.y.squeeze(1)[:args.batch_size])
+        out = model(batch.x, batch.edge_index, batch.relations)[:batch.batch_size]
+        loss = criterion(out, batch.y.squeeze(1)[:batch.batch_size])
         loss.backward()
         optimizer.step()
         model.apply(range_constraint)
 
-        total_examples += args.batch_size
-        total_loss += float(loss) * args.batch_size
-        total_correct += int((out.argmax(dim=-1) == batch.y.squeeze(1)[:args.batch_size]).sum())
+        total_examples += batch.batch_size
+        total_loss += float(loss) * batch.batch_size
+        total_correct += int((out.argmax(dim=-1) == batch.y.squeeze(1)[:batch.batch_size]).sum())
 
     epoch_acc = total_correct / total_examples
     epoch_loss = total_loss / total_examples
@@ -46,7 +44,7 @@ def train(model, train_loader, optimizer, device, criterion, args, range_constra
 
 
 @torch.no_grad()
-def test(model, test_loader, criterion, args, device):
+def test(model, test_loader, criterion, device):
     """
     validation loop. No gradient updates
     returns accuracy per epoch and loss
@@ -57,20 +55,18 @@ def test(model, test_loader, criterion, args, device):
     The following ones are the sampled Neighbors
     @param device: gpu or cpu
     @param criterion: defined loss function
-    @param args: input parameters
     """
     model.eval()
 
     total_examples = total_loss = total_correct = 0
-    for batch in test_loader:
-        # batch = batch.to(device, 'edge_index')
+    for batch in iter(test_loader):
         batch = batch.to(device)
-        out = model(batch.x, batch.edge_index, batch.relations)[:args.batch_size]
-        loss = criterion(out, batch.y.squeeze(1)[:args.batch_size])
+        out = model(batch.x, batch.edge_index, batch.relations)[:batch.batch_size]
+        loss = criterion(out, batch.y.squeeze(1)[:batch.batch_size])
 
-        total_examples += args.batch_size
-        total_correct += int((out.argmax(dim=-1) == batch.y.squeeze(1)[:args.batch_size]).sum())
-        total_loss += float(loss) * args.batch_size
+        total_examples += batch.batch_size
+        total_correct += int((out.argmax(dim=-1) == batch.y.squeeze(1)[:batch.batch_size]).sum())
+        total_loss += float(loss) * batch.batch_size
 
     epoch_acc = total_correct / total_examples
     epoch_loss = total_loss / total_examples
