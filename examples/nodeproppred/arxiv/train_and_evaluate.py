6ff6d99225ae40f47bc4050a2cfe911fc8826ca2
luisaWerner <luisa.werner@icloud.com>
2022-03-14 09:58:52 +0100

    parameter change train_and_evaluate.py

--- examples/nodeproppred/arxiv/train_and_evaluate.py
+++ examples/nodeproppred/arxiv/train_and_evaluate.py
@@ -42,7 +42,7 @@ def main():
     parser.add_argument('--es_patience', type=int, default=3)
     parser.add_argument('--sampling_neighbor_size', type=int, default=10)
     parser.add_argument('--batch_size', type=int, default=1000)
-    parser.add_argument('--full_batch', type=bool, default=False)
+    parser.add_argument('--full_batch', type=bool, default=True)
     parser.add_argument('--num_workers', type=int, default=30)
     parser.add_argument('--seed', type=int, default=100)
 
