40021a2ed1130e3b9eff99c3db54f1da39a5dd27
WERNER Luisa <luisa.werner@inria.fr>
2022-12-28 20:32:32 +0100

    mps M1 chip

--- KENN-Experiments/train_and_evaluate.py
+++ KENN-Experiments/train_and_evaluate.py
@@ -15,10 +15,13 @@ from pathlib import Path
 
 def run_experiment(args):
     torch_geometric.seed_everything(args.seed)
-    print(f"backend available {torch.backends.mps.is_available()}")
-    device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
-    device = torch.device(device)
-    print(f'Cuda available? {torch.cuda.is_available()}, Number of devices: {torch.cuda.device_count()}')
+    if args.mps:
+        print(f"MPS backend available {torch.backends.mps.is_available()}")
+        device = torch.device("mps")
+    else:
+        device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
+        device = torch.device(device)
+        print(f'Cuda available? {torch.cuda.is_available()}, Number of devices: {torch.cuda.device_count()}')
 
     print(f'Start {args.mode} Training')
     xp_stats = ExperimentStats()
