1c6b1ca04da23058ad87c2f68453ffc67079dd48
WERNER Luisa <luisa.werner@inria.fr>
2023-03-13 15:31:19 +0100

    bugfix: return statement order for test in training_batch.py

--- Experiments/train_and_evaluate.py
+++ Experiments/train_and_evaluate.py
@@ -81,7 +81,6 @@ def run_experiment(args):
         test_accuracy, valid_acc, *_ = test(model, criterion, device, evaluator)
         test_accuracies += [test_accuracy]
         evaluator.save_state_dict(valid_acc, model=model)  # todo reference with model different ?
-        # evaluator.save_clause_weights()
         rs = RunStats(run, train_losses, train_accuracies, valid_losses, valid_accuracies, test_accuracy, epoch_time,
                       test_accuracies)
         xp_stats.add_run(rs)
