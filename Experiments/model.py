b0bfb3edf8734fc7ed128a5ddf0c8783f645394c
WERNER Luisa <luisa.werner@inria.fr>
2023-03-21 16:50:46 +0100

    cleaning: remove models

--- Experiments/model.py
+++ Experiments/model.py
@@ -78,50 +78,6 @@ class _GraphSampling(torch.nn.Module):
         pass
 
 
-class LinearRegression(_GraphSampling):
-    def __init__(self, args, **kwargs):
-        super(LinearRegression, self).__init__(args)
-        self.name = 'LinearRegression'
-        self.lin = Linear(self.num_features, self.out_channels)
-        self.train_loader = NeighborLoader(T.ToInductive()(self.train_data) if self.inductive else self.train_data,
-                                           num_neighbors=self.num_neighbors[:self.num_layers_sampling],
-                                           shuffle=True,
-                                           input_nodes=None,
-                                           batch_size=len(self.data.train_mask) if self.full_batch else self.batch_size,
-                                           num_workers=self.num_workers,
-                                           transform=T.RelationsAttribute(),
-                                           neighbor_sampler=None)
-
-    def reset_parameters(self, **kwargs):
-        self.lin.reset_parameters()
-
-    def forward(self, x, edge_index, relations=None, edge_weight=None):
-        x = self.lin(x)
-        return x
-
-
-class LogisticRegression(_GraphSampling):
-    def __init__(self, args, **kwargs):
-        super(LogisticRegression, self).__init__(args)
-        self.name = 'LinearRegression'
-        self.lin = Linear(self.num_features, self.out_channels)
-        self.train_loader = NeighborLoader(T.ToInductive()(self.train_data) if self.inductive else self.train_data,
-                                           num_neighbors=self.num_neighbors[:self.num_layers_sampling],
-                                           shuffle=True,
-                                           input_nodes=None,
-                                           batch_size=len(self.data.train_mask) if self.full_batch else self.batch_size,
-                                           num_workers=self.num_workers,
-                                           transform=T.RelationsAttribute(),
-                                           neighbor_sampler=None)
-
-    def reset_parameters(self, **kwargs):
-        self.lin.reset_parameters()
-
-    def forward(self, x, edge_index, relations, edge_weight):
-        x = torch.sigmoid(self.lin(x))
-        return x
-
-
 class GAT(_GraphSampling):
     """ Implementation of GAT """
 
@@ -166,131 +122,6 @@ class GAT(_GraphSampling):
         x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.conv2(x, edge_index)
         return x
-
-
-class ClusterGCN(_GraphSampling):
-    def __init__(self, args, **kwargs):
-        super(ClusterGCN, self).__init__(args)
-        self.name = 'ClusterGCN'
-        self.parts = args.num_parts
-        self.convs = torch.nn.ModuleList()
-        self.convs.append(SAGEConv(self.num_features, self.hidden_channels))
-        for _ in range(self.num_layers - 2):
-            self.convs.append(SAGEConv(self.hidden_channels, self.hidden_channels))
-        self.convs.append(SAGEConv(self.hidden_channels, self.out_channels))
-
-        sample_size = max(1, int(self.batch_size / (self.train_data.num_nodes / self.num_parts)))
-        cluster_data = ClusterData(T.ToInductive()(self.train_data) if self.inductive else self.train_data,
-                                   num_parts=self.num_parts, recursive=False)
-        self.train_loader = ClusterLoader(cluster_data, batch_size=sample_size, shuffle=True)
-
-    def reset_parameters(self):
-        for conv in self.convs:
-            conv.reset_parameters()
-
-    def forward(self, x, edge_index, relations, edge_weight=None):
-        for i, conv in enumerate(self.convs):
-
-            x = conv(x, edge_index)  # no edge weight for SAGEConv
-            if i != self.num_layers - 1:
-                x = F.relu(x)
-                x = F.dropout(x, p=self.dropout, training=self.training)
-        return x
-
-
-class KENN_ClusterGCN(ClusterGCN):
-    """ kenn-sub with GraphSage (from ogb) as base NN"""
-    def __init__(self, args):
-        super().__init__(args)
-        self.name = str('KENN_' + self.name)
-        self.knowledge = KnowledgeGenerator(self, args).knowledge
-        self.kenn_layers = ModuleList()
-
-        for _ in range(args.num_kenn_layers):
-            self.kenn_layers.append(relational_parser(knowledge_file=self.knowledge, initial_clause_weight=args.clause_weight,
-                                                      boost_function=args.boost_function))
-
-    def reset_parameters(self):
-        """ resets parameters to default initialization: Base NN and KENN clause weights """
-        super().reset_parameters()
-        for layer in self.kenn_layers:
-            layer.reset_parameters()
-
-    def forward(self, x, edge_index, relations, edge_weight=None):
-        z = super().forward(x, edge_index, relations, edge_weight=edge_weight)
-        for layer in self.kenn_layers:
-            z, _ = layer(unary=z, edge_index=edge_index, binary=relations)
-
-        return z
-
-
-class GraphSAINT(_GraphSampling):
-    # Implemented base on https://github.com/rusty1s/pytorch_geometric/blob/master/examples/graph_saint.py
-    def __init__(self, args, **kwargs):
-        super(GraphSAINT, self).__init__(args)
-        self.name = 'GraphSAINT'
-        self.num_steps = args.num_steps
-        self.walk_length = args.walk_length
-        self.use_norm = args.use_norm
-        self.sample_covreage = args.sample_coverage
-        self.aggr = "add" if self.use_norm else "mean"
-        self.convs = ModuleList()
-        self.convs.append(SAGEConv(self.num_features, self.hidden_channels))
-        for _ in range(self.num_layers - 2):
-            conv = SAGEConv(self.hidden_channels, self.hidden_channels)
-            conv.aggr = self.aggr
-            self.convs.append(conv)
-        self.lin = Linear(self.hidden_channels, self.out_channels)
-
-        self.train_loader = RWSampler(
-            T.ToInductive()(self.train_data) if self.inductive else self.train_data,
-            batch_size=self.batch_size,
-            walk_length=self.walk_length,
-            num_steps=self.num_steps,
-            num_workers=self.num_workers,
-            sample_coverage=self.sample_coverage,
-        )
-
-    def reset_parameters(self):
-        self.lin.reset_parameters()
-        for conv in self.convs:
-            conv.reset_parameters()
-
-    def forward(self, x, edge_index, relations, edge_weight=None):
-        for i, conv in enumerate(self.convs):
-            x = conv(x, edge_index)  # no edge_weight for SAGEConv
-            if i != len(self.convs) - 1:
-                x = F.relu(x)
-                x = F.dropout(x, p=self.dropout, training=self.training)  # todo is self.training activated?
-        x = self.lin(x)
-        return x
-
-
-class KENN_GraphSAINT(GraphSAINT):
-    """ kenn-sub with GraphSage (from ogb) as base NN"""
-
-    def __init__(self, args):
-        super().__init__(args)
-        self.name = str('KENN_' + self.name)
-        self.knowledge = KnowledgeGenerator(self, args).knowledge
-        self.kenn_layers = ModuleList()
-
-        for _ in range(args.num_kenn_layers):
-            self.kenn_layers.append(relational_parser(knowledge_file=self.knowledge, initial_clause_weight=args.clause_weight,
-                                                      boost_function=args.boost_function))
-
-    def reset_parameters(self):
-        """ resets parameters to default initialization: Base NN and KENN clause weights """
-        super().reset_parameters()
-        for layer in self.kenn_layers:
-            layer.reset_parameters()
-
-    def forward(self, x, edge_index, relations, edge_weight=None):
-        z = super().forward(x, edge_index, relations, edge_weight=edge_weight)
-        for layer in self.kenn_layers:
-            z, _ = layer(unary=z, edge_index=edge_index, binary=relations)
-
-        return z
 
 
 class GCN(_GraphSampling):
@@ -338,45 +169,6 @@ class GCN(_GraphSampling):
         return x
 
 
-class SAGE(_GraphSampling):
-    # TODO compare differences to :https://github.com/VITA-Group/Large_Scale_GCN_Benchmarking/blob/6a3b91c7bdd0459f454c92a364bca2a69e26cda4/GraphSampling/GraphSAGE.py
-    """ Implementation of GraphSAGE - ogb baseline  """
-
-    def __init__(self, args, **kwargs):
-        super(SAGE, self).__init__(args)
-        self.name = 'SAGE'
-        self.convs = ModuleList()
-        self.convs.append(SAGEConv(self.num_features, self.hidden_channels))
-        self.bns = ModuleList()
-        self.bns.append(BatchNorm1d(self.hidden_channels))
-        for _ in range(self.num_layers - 2):
-            self.convs.append(SAGEConv(self.hidden_channels, self.hidden_channels))
-            self.bns.append(BatchNorm1d(self.hidden_channels))
-        self.convs.append(SAGEConv(self.hidden_channels, self.out_channels))
-        self.train_loader = NeighborLoader(T.ToInductive()(self.train_data),  # always inductive with graphSAGE
-                                           num_neighbors=self.num_neighbors[:self.num_layers_sampling],
-                                           shuffle=True,
-                                           input_nodes=self.train_data.train_mask,  # todo is this needed ?
-                                           batch_size=self.batch_size,
-                                           num_workers=self.num_workers,
-                                           transform=T.RelationsAttribute(),
-                                           neighbor_sampler=None)
-
-    def reset_parameters(self):
-        for conv in self.convs:
-            conv.reset_parameters()
-        for bn in self.bns:
-            bn.reset_parameters()
-
-    def forward(self, x, edge_index, relations, edge_weight=None):
-        for i, conv in enumerate(self.convs[:-1]):
-            x = conv(x, edge_index)  # no edge_weight for SAGEConv
-            x = self.bns[i](x)
-            x = F.relu(x)
-            x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.convs[-1](x, edge_index)
-        return x
-
 
 class MLP(_GraphSampling):
     """ MLP baseline for OGB """
@@ -395,9 +187,7 @@ class MLP(_GraphSampling):
         self.lins.append(Linear(self.hidden_channels, self.out_channels))
 
         if args.load_baseNN:
-            # todo verify if the correct state dict is loaded etc.
-            # todo debug why this has very bad performance
-            # todo : instead of loading baseNN as function: load preactivations instead of features and continue with them !!! (that's how it's done in alessandros code)
+
             if not args.model.startswith('KENN'):
                 raise ValueError('Cannot train a pretrained-base NN. Set load_baseNN to False if used standalone.')
             else:
@@ -433,43 +223,6 @@ class MLP(_GraphSampling):
             x = F.relu(x)
             x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.lins[-1](x)
-        return x
-
-
-class Standard(_GraphSampling):
-    """ Original Base Neural Network by kenn-sub paper """
-
-    hidden_channels = 50
-    out_channels = 6
-    dropout = 0.5
-
-    def __init__(self, args, **kwargs):
-        super(Standard, self).__init__(args)
-        self.name = 'Standard'
-        self.lin_layers = ModuleList()
-        self.lin_layers.append(Linear(self.in_channels, self.hidden_channels))
-        self.lin_layers.append(Linear(self.hidden_channels, self.hidden_channels))
-        self.lin_layers.append(Linear(self.hidden_channels, self.hidden_channels))
-        self.lin_layers.append(Linear(self.hidden_channels, self.out_channels))
-        self.train_loader = NeighborLoader(T.ToInductive()(self.train_data) if self.inductive else self.train_data,
-                                           num_neighbors=self.num_neighbors[:self.num_layers_sampling],
-                                           shuffle=True,
-                                           input_nodes=None,
-                                           batch_size=len(self.data.train_mask) if self.full_batch else self.batch_size,
-                                           num_workers=self.num_workers,
-                                           transform=T.RelationsAttribute(),
-                                           neighbor_sampler=None)
-
-    def reset_parameters(self):
-        for layer in self.lin_layers:
-            layer.reset_parameters()
-
-    def forward(self, x, edge_index=None, relations=None, edge_weight=None):
-        for i, lin in enumerate(self.lin_layers[:-1]):
-            x = lin(x)
-            x = F.relu(x)
-            x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.lin_layers[-1](x)
         return x
 
 
@@ -526,33 +279,8 @@ class KENN_MLP(MLP):
         return z
 
 
-class KENN_SAGE(SAGE):
-    """ kenn-sub with GraphSage (from ogb) as base NN"""
-
-    def __init__(self, args):
-        super().__init__(args)
-        self.name = str('KENN_' + self.name)
-        self.knowledge = KnowledgeGenerator(self, args).knowledge
-        self.kenn_layers = ModuleList()
-
-        for _ in range(args.num_kenn_layers):
-            self.kenn_layers.append(relational_parser(knowledge_file=self.knowledge,
-                                                      initial_clause_weight=args.clause_weight, boost_function=args.boost_function))
-
-    def reset_parameters(self):
-        super().reset_parameters()  # should call reset parameter function of MLP
-        for layer in self.kenn_layers:
-            layer.reset_parameters()
-
-    def forward(self, x, edge_index, relations, edge_weight=None):
-        z = super().forward(x, edge_index, relations, edge_weight=edge_weight)
-        for layer in self.kenn_layers:
-            z, _ = layer(unary=z, edge_index=edge_index, binary=relations)
-        return z
-
-
 class KENN_GAT(GAT):
-    """ kenn-sub with GraphSage (from ogb) as base NN"""
+    """ kenn-sub with GAT as base NN """
 
     def __init__(self, args):
         super().__init__(args)
@@ -577,51 +305,4 @@ class KENN_GAT(GAT):
         return z
 
 
-class KENN_LogisticRegression(LogisticRegression):
-    """ kenn-sub with MLP (from ogb) as base NN"""
 
-    def __init__(self, args):
-        super().__init__(args)
-        self.name = str('KENN_' + self.name)
-        self.knowledge = KnowledgeGenerator(self, args).knowledge
-        self.kenn_layers = ModuleList()
-
-        for _ in range(args.num_kenn_layers):
-            self.kenn_layers.append(relational_parser(knowledge_file=self.knowledge, initial_clause_weight=args.clause_weight,
-                                                      boost_function=args.boost_function))
-
-    def reset_parameters(self):
-        super().reset_parameters()
-        for layer in self.kenn_layers:
-            layer.reset_parameters()
-
-    def forward(self, x, edge_index, relations, edge_weight):
-        z = super().forward(x, edge_index, relations, edge_weight=edge_weight)
-        for layer in self.kenn_layers:
-            z, _ = layer(unary=z, edge_index=edge_index, binary=relations)
-        return z
-
-
-class KENN_LinearRegression(LinearRegression):
-    """ kenn-sub with MLP (from ogb) as base NN"""
-
-    def __init__(self, args):
-        super().__init__(args)
-        self.name = str('KENN_' + self.name)
-        self.knowledge = KnowledgeGenerator(self, args).knowledge
-        self.kenn_layers = ModuleList()
-
-        for _ in range(args.num_kenn_layers):
-            self.kenn_layers.append(relational_parser(knowledge_file=self.knowledge, initial_clause_weight=args.clause_weight,
-                                                      boost_function=args.boost_function))
-
-    def reset_parameters(self):
-        super().reset_parameters()
-        for layer in self.kenn_layers:
-            layer.reset_parameters()
-
-    def forward(self, x, edge_index, relations=None, edge_weight=None):
-        z = super().forward(x, edge_index, relations, edge_weight=edge_weight)
-        for layer in self.kenn_layers:
-            z, _ = layer(unary=z, edge_index=edge_index, binary=relations)
-        return z
